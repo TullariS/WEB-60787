9140b297e911775d0abb32dda29a4f50c78efa86
João Almeida <joao@insiteability.com>
2023-04-25 19:39:40 +0100

    Added test files

---
+++ TestTs.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>
+    {{ prop1.strVar }}
+    {{ prop2.strVar2 }}
+    {{ prop1Obj.strVar }}
+    {{ prop2Obj.strVar2 }}
+  </p>
+</template>
+<script lang="ts">
+import { PropType } from 'vue'
+import Prop2 from './types'
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  props: {
+    prop1: Object as PropType<{strVar: string | string[]}>,
+    prop2: Object as PropType<Prop2>,
+  },
+  data () {
+    return {
+      prop1Obj: this.prop1,
+      prop2Obj: this.prop2,
+
+      prop1Val: this.prop1.strVar,
+      prop2Val: this.prop2.strVar2,
+    }
+  },
+}
+</script>
---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare interface Prop2 {
+  strVar2: string | string[]
+}
+
+declare interface SomeOtherProp {
+  strVar2: number
+}
+
+export default Prop2
